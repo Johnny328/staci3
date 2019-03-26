06d2413acce8073b13353882375e86f8a4653806
Richard Weber <weberrichard92@gmail.com>
2019-03-26 14:17:35 +0100

    Delete StaciException.h

--- StaciException.h
+++
@@ -1,7 +0,0 @@
-class StaciException{
-private:
- string hibauzenet; //verbal description of the error
-public:
- StaciException(string d):hibauzenet(d){}
- string getDescription() {return hibauzenet;}
-};
