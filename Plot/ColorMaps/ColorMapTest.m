56fc1412d164a45833af734bf570e20322964f38
Richard Weber <weberrichard92@gmail.com>
2019-08-23 16:30:16 +0200

    Add files via upload

---
+++ Plot/ColorMaps/ColorMapTest.m
@@ -0,0 +1,16 @@
+clear;
+figure;hold on;
+%set
+markerSize = 10;
+files = dir('*.txt');
+for i=1:length(files)
+    colorMap = importdata([files(i).name]);
+    for j=1:length(colorMap)
+        plot(i,colorMap(j,1),'ko','markersize',markerSize,'linewidth',2,'markerfacecolor',[colorMap(j,2),colorMap(j,3),colorMap(j,4)],'color',[colorMap(j,2),colorMap(j,3),colorMap(j,4)])
+    end
+    legendName{i} = files(i).name(1:end-4);
+end
+
+set(gcf, 'Position',  [200, 200, 800, 400]);
+legend(legendName);
+xlim([0,20]);
